df56ac1c765de7af3704e757c483efa59b31d253
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-27 18:25:51 +0700

    commit13

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -30,3 +30,16 @@ void orang::setNama(string pNama)
 {
     nama = pNama;
 }
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("joko susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
