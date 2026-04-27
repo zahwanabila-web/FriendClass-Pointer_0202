9c45c90feccc655c0f62289119778cf8d5549299
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-27 17:49:40 +0700

    commit5

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -10,3 +10,12 @@ class mahasiswa
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1};
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
