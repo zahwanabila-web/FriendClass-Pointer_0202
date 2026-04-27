a7db984bc57a123aa000af59c95852b481e38e81
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-27 18:18:04 +0700

    commit11

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -9,4 +9,20 @@ private:
 
 public:
     void setNama(string pNama);
+    friend class siswa;
 };
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
