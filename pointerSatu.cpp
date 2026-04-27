6a5aa37cbf41a4095c3bc0f8da42c7499bd5b42f
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-27 17:41:00 +0700

    commit2

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -4,6 +4,13 @@ class mahasiswa{
     public:
     int nim;
     void showNim(){
-        
+        cout<<"No Induk = "<<nim<<endl;
     }
 };
+
+int main (){
+    mahasiswa mhs{1};
+    mhs.showNim();
+
+    mahasiswa &refMhs = mhs;
+}
