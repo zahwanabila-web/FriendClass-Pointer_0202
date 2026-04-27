e304195ce99a9b5c5aeec68d2517f73db0b8decb
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-27 17:55:01 +0700

    commit7

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -3,4 +3,12 @@ using namespace std;
 class mahasiswa
 {
     string nama;
+    public:
+    friend void setNama(mahasiswa &a, string);
 };
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
